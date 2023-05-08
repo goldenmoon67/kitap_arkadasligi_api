6754b94b98915ef710bf89d137a904288d0a99a3
goldenmoon67 <abdulmirac@gmail.com>
2023-05-09 01:29:19 +0300

    basic CRUD

---
+++ models/model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+
+    email: {
+        required: true,
+        type: String
+    },
+   
+})
+
+module.exports = mongoose.model('User', dataSchema)
---
+++ routes/routes.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const Model = require('../models/model');
+const router = express.Router()
+
+router.post('/create', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        email: req.body.email
+    });
+
+    try {
+        const dataToSave =await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+});
+
+
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+module.exports = router;
