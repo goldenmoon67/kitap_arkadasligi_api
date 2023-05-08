fb8cc674e94a7e8f5ffbae674b1e762fb3bf3cce
goldenmoon67 <abdulmirac@gmail.com>
2023-05-09 02:21:55 +0300

    firebase admin and signup

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const admin = require("firebase-admin");
+const credentials = require("./serviceAccountKey.json");
 const routes = require('./routes/routes');
 
 const mongoString = process.env.DATABASE_URL;
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+});
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -18,9 +23,24 @@ database.once('connected', () => {
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.post('/register', async (req, res) => {
+
+    const userResponse = await admin.auth().createUser({
+        email: req.body.email,
+        password: req.body.password,
+        emailVerified: false,
+        disabled: false
+    });
+
+    res.json(userResponse);
+});
+
+
+
 app.use('/api', routes)
 
-
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server Started at ${8080}`)
 })
